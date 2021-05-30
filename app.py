c70632e33cf110879ed47ae86392e4ecbdef04aa
joaopp <joao.pp@gmail.com>
2021-05-30 10:29:19 +0100

    v1

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def hello():
     return 'Hello World'
 
+@app.route('/getdata', methods=['GET'])
+def getdata():
+    print (request.form)
+    return 'Hello World'
+
 if __name__ == '__main__':
     app.run()
